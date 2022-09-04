3c46d2449cba7a49a47c8440d7ef8dd15ee07a29
MarkViews <46404728+markviews@users.noreply.github.com>
2022-09-03 19:37:19 -0700

    Update sort by room.js

--- sort by room.js
+++ sort by room.js
@@ -1,53 +1,76 @@
 var list = document.getElementsByTagName("tr");
-var map = new Map();
+var buildings = {};
 var lastCRN = "";
 
 for (var i = 0; i < list.length; i++) {
-var sub = list[i].childNodes;
-if (sub.length == 45) {
-	
-	var loc = sub[27].childNodes[0].innerHTML;
-	var time = sub[25].childNodes[0].innerHTML;
-	var crn = sub[5].childNodes[1].innerHTML.trim();
-	var mon = sub[11].childNodes[0].innerHTML;
-	var tue = sub[13].childNodes[0].innerHTML;
-	var wed = sub[15].childNodes[0].innerHTML;
-	var thu = sub[17].childNodes[0].innerHTML;
-	var fri = sub[19].childNodes[0].innerHTML;
-	var sat = sub[21].childNodes[0].innerHTML;
-	var sun = sub[23].childNodes[0].innerHTML;
+	var sub = list[i].childNodes;
+	if (sub.length == 45) {
+		
+		var loc = sub[27].childNodes[0].innerHTML;
+		var time = sub[25].childNodes[0].innerHTML;
+		var crn = sub[5].childNodes[1].innerHTML.trim();
+		var mon = sub[11].childNodes[0].innerHTML;
+		var tue = sub[13].childNodes[0].innerHTML;
+		var wed = sub[15].childNodes[0].innerHTML;
+		var thu = sub[17].childNodes[0].innerHTML;
+		var fri = sub[19].childNodes[0].innerHTML;
+		var sat = sub[21].childNodes[0].innerHTML;
+		var sun = sub[23].childNodes[0].innerHTML;
 
-	lastCRN = crn;
-	var stuff = [];
-	if (map.get(loc) != undefined)
-		stuff = map.get(loc);
-	stuff.push(mon + tue + wed + thu + fri + sat + sun + " " + time + " "  + crn);
-	map.set(loc, stuff);
-} else if (sub.length == 29) {
-	var loc = sub[21].childNodes[0].innerHTML;
-	var time = sub[19].childNodes[0].innerHTML;
-	var mon = sub[5].childNodes[0].innerHTML;
-	var tue = sub[7].childNodes[0].innerHTML;
-	var wed = sub[9].childNodes[0].innerHTML;
-	var thu = sub[11].childNodes[0].innerHTML;
-	var fri = sub[13].childNodes[0].innerHTML;
-	var sat = sub[15].childNodes[0].innerHTML;
-	var sun = sub[17].childNodes[0].innerHTML;
+		if (loc == undefined) loc = "undefined"
+		var building = loc.split(" ")[0];
+		var map = buildings[building];
+		
+		if (map == undefined) {
+			map = new Map();
+			buildings[building] = map;
+		}
 
-	var stuff = [];
-	if (map.get(loc) != undefined)
-		stuff = map.get(loc);
-	stuff.push(mon + tue + wed + thu + fri + sat + sun + " " + time + " "  + lastCRN);
-	map.set(loc, stuff);
-}
+		lastCRN = crn;
+		var stuff = [];
+		if (map.get(loc) != undefined)
+			stuff = map.get(loc);
+		stuff.push(mon + tue + wed + thu + fri + sat + sun + " " + time + " "  + crn);
+		map.set(loc, stuff);
+	} else if (sub.length == 29) {
+		var loc = sub[21].childNodes[0].innerHTML;
+		var time = sub[19].childNodes[0].innerHTML;
+		var mon = sub[5].childNodes[0].innerHTML;
+		var tue = sub[7].childNodes[0].innerHTML;
+		var wed = sub[9].childNodes[0].innerHTML;
+		var thu = sub[11].childNodes[0].innerHTML;
+		var fri = sub[13].childNodes[0].innerHTML;
+		var sat = sub[15].childNodes[0].innerHTML;
+		var sun = sub[17].childNodes[0].innerHTML;
+
+		if (loc == undefined) loc = "undefined"
+		var building = loc.split(" ")[0];
+		var map = buildings[building];
+		
+		if (map == undefined) {
+			map = new Map();
+			buildings[building] = map;
+		}
+
+		var stuff = [];
+		if (map.get(loc) != undefined)
+			stuff = map.get(loc);
+		stuff.push(mon + tue + wed + thu + fri + sat + sun + " " + time + " "  + lastCRN);
+		map.set(loc, stuff);
+	}
 }
 
 var str = "";
 
-for (let [key, value] of map.entries()) {
-	str = str + key + "\n";
-for (var i = 0; i < value.length; i++)
-	str = str + "\t" + value[i] + "\n";
+for(var building in buildings) {
+	var map = buildings[building];
+
+	for (let [key, value] of map.entries()) {
+		str = str + key + "\n";
+		for (var i = 0; i < value.length; i++)
+			str = str + "\t" + value[i] + "\n";
+	}
+	
 }
 
 console.log(str);
